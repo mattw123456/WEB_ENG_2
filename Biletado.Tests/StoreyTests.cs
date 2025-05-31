62083e9db88141213677f4bcab06d1d18742a73d
Finn <Finn.Marin@Berthold.com>
2025-05-31 16:34:36 +0200

    Ansichtproblem

--- Biletado.Tests/StoreyTests.cs
+++ Biletado.Tests/StoreyTests.cs
@@ -1,37 +1,20 @@
-﻿using Biletado.Domain.Storey;using Moq;namespace Biletado.Tests{    public class StoreyTests    {        private Mock<IStoreyService> _storeyServiceMock;        public StoreyTests()        {            _storeyServiceMock = new Mock<IStoreyService>();        }        [Fact]        public async Task CreateNewAsync_ShouldReturnStorey_WhenCreationSucceeds() {            // Arrange            var storeyId = Guid.NewGuid();            var buildingId = Guid.NewGuid();            var newStorey = new Storey {                id = storeyId,                name = "Test",                building_id = buildingId            };            _storeyServiceMock.Setup(service => service.CreateStoreyAsync(newStorey)).ReturnsAsync(newStorey);            // Act            var result = await _storeyServiceMock.Object.CreateStoreyAsync(newStorey);            // Assert            Assert.Equal(newStorey, result);        }
-
-        [Fact]
-        public async Task GetAllStoreysAsync_ShouldReturnListOfStoreys_ForGivenBuildingId()
+﻿using Biletado.Domain.Storey;using Moq;namespace Biletado.Tests{    public class StoreyTests    {        private Mock<IStoreyService> _storeyServiceMock;        public StoreyTests()        {            _storeyServiceMock = new Mock<IStoreyService>();        }        [Fact]        public async Task CreateNewAsync_ShouldReturnStorey_WhenCreationSucceeds()
         {
+
             // Arrange
-            var buildingId = Guid.NewGuid();
-            var storeyId = Guid.NewGuid();
+            var storeyId = Guid.NewGuid();            var buildingId = Guid.NewGuid();            var newStorey = new Storey
+            {                id = storeyId,                name = "Test",                building_id = buildingId            };            _storeyServiceMock.Setup(service => service.CreateStoreyAsync(newStorey)).ReturnsAsync(newStorey);
 
-            var expectedStoreys = new List<Storey> {
+            // Act
+            var result = await _storeyServiceMock.Object.CreateStoreyAsync(newStorey);
 
-                new Storey
-                {
-                    id = storeyId,
-                    name = "TestStorey",
-                    building_id = buildingId
-                }
-            };
-   
-
-            _storeyServiceMock
-                .Setup(service => service.GetAllStoreysAsync(buildingId, false))
-                .ReturnsAsync(expectedStoreys);
+            // Assert
+            Assert.Equal(newStorey, result);        }        [Fact]        public async Task GetAllStoreysAsync_ShouldReturnListOfStoreys_ForGivenBuildingId()        {
+            // Arrange
+            var buildingId = Guid.NewGuid();            var storeyId = Guid.NewGuid();            var expectedStoreys = new List<Storey> {                new Storey                {                    id = storeyId,                    name = "TestStorey",                    building_id = buildingId                }            };            _storeyServiceMock                .Setup(service => service.GetAllStoreysAsync(buildingId, false))                .ReturnsAsync(expectedStoreys);
 
             // Act
             var result = await _storeyServiceMock.Object.GetAllStoreysAsync(buildingId, false);
 
             // Assert
-            Assert.NotNull(result);
-            Assert.Single(result);
-            Assert.Equal(expectedStoreys[0].id, result[0].id);
-            Assert.Equal(expectedStoreys[0].name, result[0].name);
-            Assert.Equal(expectedStoreys[0].building_id, result[0].building_id);
-
-            _storeyServiceMock.Verify(s => s.GetAllStoreysAsync(buildingId, false), Times.Once);
-        }
-    }}
+            Assert.NotNull(result);            Assert.Single(result);            Assert.Equal(expectedStoreys[0].id, result[0].id);            Assert.Equal(expectedStoreys[0].name, result[0].name);            Assert.Equal(expectedStoreys[0].building_id, result[0].building_id);            _storeyServiceMock.Verify(s => s.GetAllStoreysAsync(buildingId, false), Times.Once);        }    }}
